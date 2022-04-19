04eb4b66a29ebf883fb346eb1fee4c4f2ae2e98b
Greg Brewton <greg@studio42dev.com>
2022-04-19 12:00:42 -0600

    Writes test for logo render

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders App component', () => {
-  render(<App />);
-  const app = screen.getByTestId('App');
-  expect(app).toBeInTheDocument();
-});
+describe("App component", () => {
+  it('renders App component', () => {
+    render(<App />);
+    const app = screen.getByTestId('App');
+    expect(app).toBeInTheDocument();
+  });
+
+  it("Renders the Studio42 Dev logo", () => {
+    render(<App />);
+    const logo = screen.getByAltText(/Studio42 rocket logo/i);
+    expect(logo).toBeInTheDocument();
+  })
+})
